f8865ae92af7a2376dc6262d41fcc74d49eb8915
paksewon <qlrhdro2002@naver.com>
2023-12-13 16:33:21 +0900

    강의실 블랙잭

---
+++ Blcakjack_Project/src/com/projeck/teacher/model/PlayingCardDto.java
@@ -0,0 +1,19 @@
+package com.projeck.teacher.model;
+
+public class PlayingCardDto {
+		public String suit;
+		public String denomination;
+		public int value;
+		
+		public String[] getPattern() {
+			String pattern[] = {
+				String.format("┌──────┐ "),
+				String.format("│  %1s   │ ",this.suit),
+				String.format("│      │ "),
+				String.format("│      │ "),
+				String.format("│  %2s  │ ",this.denomination),
+				String.format("└──────┘ ")
+			};
+			return pattern;
+		}
+}
